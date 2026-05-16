1185a6fbe9fa59561a89b414cf547ae76b502b6b
SofiaDyLaFuente <sofia.dylafuente@gmail.com>
2026-05-16 14:41:16 -0300

    refactor: Implementação da main no local correto.

--- src/main.cpp
+++ src/main.cpp
@@ -5,3 +5,32 @@ Alunos: João Vitor Pereira Vieira,
         Sofia Dy La Fuente Monteiro.
 */
 
+#include <string>
+#include "../include/PreProcessor.h"
+#include "../include/Assembler.h"
+#include "../include/Simulator.h"
+
+int main(int argc, char* argv[]) {
+    std::string nomeArquivo = argv[1];
+
+    size_t finalArquivo = nomeArquivo.rfind('.');
+    std::string extensao = nomeArquivo.substr(finalArquivo);
+    std::string caminho = "files/" + nomeArquivo;
+
+    if (extensao == ".asm") {
+        preProcessor(caminho);
+    }
+    else if (extensao == ".pre") {
+        //montador();
+        return 0;
+    }
+    else if (extensao == ".obj") {
+        //simulador();
+        return 0;
+    }
+    else {
+        return 0;
+    }
+
+    return 0;
+}
